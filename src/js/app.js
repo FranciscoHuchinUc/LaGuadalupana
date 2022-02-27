ce826f7a0bf201a35a38edae369eb58c75e4ee6f
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-02-26 18:13:32 -0600

    La Guadalupana

---
+++ src/js/app.js
@@ -0,0 +1,152 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 800) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== FILTER PRODUCTS ===============*/
+let mixerProducts = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+mixerProducts.filter('.sourdough');
+
+const linkProducts = document.querySelectorAll('.products__item')
+linkProducts.forEach(n => n.addEventListener('click', activeProducts))
+
+function activeProducts(){
+    linkProducts.forEach(n => n.classList.remove('active-product'))
+    this.classList.add('active-product')
+}
+
+/*=============== SWIPER TESTIMONIALS ===============*/
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+    },
+});
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== LOADER ===============*/
+onload = () => {
+    const load = document.getElementById('load')
+
+    setTimeout(() =>{
+        load.style.display = 'none'
+    }, 2500)
+
+}
+
+
+/*=============== API SEND DATA FORM CONTACT ===============*/
+const $form = document.querySelector('#form');
+
+$form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(e) {
+    event.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    if(response.ok){
+        this.reset()
+        toastAlert('success', 'Formulario enviado correctamente')
+    } else if(response.status === 422){
+        toastAlert('error', 'Formulario no válido')
+    }
+}
+
+function toastAlert (icon, title) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true
+    })
+      
+    Toast.fire({
+        icon: icon,
+        title: title
+    })
+}
