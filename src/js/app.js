1354a8294239729fcd873c05001df73337e5aa0c
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-03-03 10:32:04 -0600

    Redesign Home Section

--- src/js/app.js
+++ src/js/app.js
@@ -53,7 +53,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  if (this.scrollY >= 800) header.classList.add("scroll-header");
+  if (this.scrollY >= 10) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
